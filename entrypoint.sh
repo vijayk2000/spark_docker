a121f8e10a7efef9ea15fb86642f6c67962abf3a
Vijay Krishnan V <vijaykrishnan90@yahoo.com>
2024-09-21 08:03:34 +0530

    created a shell script

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+# The shell script entrypoint.sh contains which shell script the container should run once it starts, 
+# depending on the argument provided through the docker-compose file. The whole script is pretty simple, 
+# get the workload that we want from the argument and depending on the value, execute the appropriate Spark script:
+
+#!/bin/bash
+
+SPARK_WORKLOAD=$1
+
+echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
+
+if [ "$SPARK_WORKLOAD" == "master" ];
+then
+  start-master.sh -p 7077
+elif [ "$SPARK_WORKLOAD" == "worker" ];
+then
+  start-worker.sh spark://spark-master:7077
+elif [ "$SPARK_WORKLOAD" == "history" ]
+then
+  start-history-server.sh
+fi
